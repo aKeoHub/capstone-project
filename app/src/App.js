85b4c0e5335a2bd2a7af6dbde1c68103a82126ed
Andy Keobounphan <andykeo@live.ca>
2022-06-28 14:05:03 -0600

    Added register UI, and some css.

--- app/src/App.js
+++ app/src/App.js
@@ -1,57 +1,141 @@
 import logo from './logo.svg';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
 
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
+    const [username, setUsernameReg] = useState('');
+    const [password, setPasswordReg] = useState('');
+    const [firstname, setFirstnameReg] = useState('');
+    const [lastname, setLastnameReg] = useState('');
+    const [email, setEmailReg] = useState('');
 
-    fetch('api/v1/user')
-        .then(response => response.json())
-        .then(data => {
-          setUsers(data);
-          setLoading(false);
-        })
-  }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+    const [userList, setUserList] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-intro">
-              <h2>User List</h2>
-              {users.map(user =>
-                  <div key={user.id}>
-                      <table>
-                          <tr>
-                              <th>ID</th>
-                              <th>Username</th>
-                              <th>Name</th>
-                              <th>Email</th>
-                          </tr>
-                          <tr>
-                              <td>{user.id}</td>
-                              <td>{user.userName}</td>
-                              <td>{user.firstName} {user.lastName}</td>
-                              <td>{user.email}</td>
-                          </tr>
+    useEffect(() => {
+        setLoading(true);
 
-                      </table>
-                  </div>
-              )}
-          </div>
-      </header>
-    </div>
-  );
+        fetch('api/v1/user')
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data);
+                setLoading(false);
+            })
+    }, []);
+
+    const getUsers = () => {
+        fetch.get('http://localhost:8080/api/v1/user', {}).then((response) => {
+            setUserList(response.data)
+        });
+    };
+
+    const register = () => {
+        fetch.post('http://localhost:8080/api/v1/user', {
+            username: username,
+            password: password,
+        }).then(() => {
+            console.log("Success");
+        });
+    };
+
+    // fetch("http://localhost:8080/api/v1/user", {
+    //
+    //     // Adding method type
+    //     method: "POST",
+    //
+    //     // Adding body or contents to send
+    //     body: JSON.stringify({
+    //         userId: userId,
+    //         userName: username,
+    //         password: password,
+    //         firstName: firstName,
+    //         lastName: lastName,
+    //         email: email
+    //
+    //
+    //     }),
+
+    // Adding headers to the request
+    //       headers: {
+    //           "Content-type": "application/json; charset=UTF-8"
+    //       }
+    //   })
+    //
+    //       // Converting to JSON
+    //       .then(response => response.json())
+    //
+    //       // Displaying results to console
+    //       .then(json => console.log(json));
+    //
+    // if (loading) {
+    //   return <p>Loading...</p>;
+    // }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div className="App-intro">
+                    <div className="registration">
+                        <h1>Registration</h1>
+                        <label>Username:</label>
+                        <input type="text" onChange={(e) => {
+                            setUsernameReg(e.target.value);
+                        }}
+                        />
+                        <label>Password:</label>
+                        <input type="text" onChange={(e) => {
+                            setPasswordReg(e.target.value);
+                        }}
+                        />
+                        <label>FirstName:</label>
+                        <input type="text" onChange={(e) => {
+                            setFirstnameReg(e.target.value);
+                        }}
+                        />
+                        <label>LastName:</label>
+                        <input type="text" onChange={(e) => {
+                            setLastnameReg(e.target.value);
+                        }}
+                        />
+                        <label>Email:</label>
+                        <input type="text" onChange={(e) => {
+                            setEmailReg(e.target.value);
+                        }}
+                        />
+                        <button onClick={register}>Register</button>
+                    </div>
+
+
+                    <h2>User List</h2>
+                    {users.map(user =>
+                        <div key={user.id}>
+                            <table>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Username</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                                <tr>
+                                    <td>{user.id}</td>
+                                    <td>{user.userName}</td>
+                                    <td>{user.firstName} {user.lastName}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+
+                            </table>
+                        </div>
+                    )}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
