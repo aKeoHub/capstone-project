fd124dfde935c031a7bf49498eab0faa60078586
Andy Keobounphan <andykeo@live.ca>
2022-06-30 23:09:35 -0600

    Update SignUpButton.js
    Changed location to connect to my registerpage.

--- app/src/components/SignUpButton.js
+++ app/src/components/SignUpButton.js
@@ -20,7 +20,7 @@ export const SignUpButton = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
-        <Link to='/signup' className='btn-mobile'>
+        <Link to='/registration' className='btn-mobile'>
             <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                 onClick={onClick}
