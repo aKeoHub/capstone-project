5a61bd9d43bfac59e80ecee7be2a214c82f6b655
Melvin Chew <melvincv15@gmail.com>
2022-07-01 22:42:07 -0600

    using big-react-calendar

--- app/src/App.js
+++ app/src/App.js
@@ -1,65 +1,70 @@
-import logo from './logo.svg';
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import Calendar from 'react-calendar'
-import DatePicker from 'react-date-picker';
-import ReactDOM from 'react-dom';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
 
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 6, 0),
+        end: new Date(2021, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 6, 7),
+        end: new Date(2021, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2021, 6, 20),
+        end: new Date(2021, 6, 23),
+    },
+];
 
 function App() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
 
-    const [value, onChange] = useState(new Date());
-    const [date, setDate] = useState(new Date());
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [calendarText, setCalendarText] = useState(`No Date is selected`);
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
+    }
 
-    const onDateChange = (newDate) => {
-        setDate(newDate);
-        const selectedDate = newDate;
-        console.log(newDate.toLocaleDateString());
-        setCalendarText(`Date: ${newDate.toLocaleDateString()}`);
-      }
+    return (
+        <div className="App">
+            <h1>Events</h1>
 
-return (
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 600, margin: "50px" }} />
 
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="card card-outline card-info">
-                <div class="card-header">
-                    <h3 class="card-title">Calendar</h3>
-                </div>
-
-                <div class="card-body  ">
-                    <div id="Calendar_wrapper">
-                        <div class="row">
-                            <div class="col-md center">
-                                <Calendar
-                                  onChange={onDateChange}
-                                  value={date}
-                                  showNeighboringMonth={false}
-                                  locale={"en-US"}
-                                  class="calendarDiv"
-                                />
-                            </div>
-                            <div class="col-md center">
-                                <h5 class="calendarDiv">{calendarText}</h5>
-                            </div>
-                        </div>
+                    <div>
+                    <h2>Add New Event</h2>
+                        <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                        <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                        <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                        <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                            Add Event
+                        </button>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-   );
+                     </div>
+    );
 }
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+
 export default App;
