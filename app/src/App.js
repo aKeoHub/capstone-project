df0805b805d4b63eb85ad3d3649b4007994330f6
Melvin Chew <melvincv15@gmail.com>
2022-07-03 22:33:27 -0600

    added forms, dropdown

--- app/src/App.js
+++ app/src/App.js
@@ -12,6 +12,8 @@ function App() {
     const [date, setDate] = useState(new Date());
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [category, setCategory] = useState([]);
+    const [categoryText, setCategoryText] = useState(`No Category is selected`);
     const [calendarText, setCalendarText] = useState(`No Date is selected`);
 
     const onDateChange = (newDate) => {
@@ -20,6 +22,11 @@ function App() {
         console.log(newDate.toLocaleDateString());
         setCalendarText(`Date: ${newDate.toLocaleDateString()}`);
       }
+
+    const onCategoryChange = (category) => {
+        setCategory(category);
+        setCategoryText(`${category.toString()}`);
+    }
 
 return (
 
@@ -32,7 +39,7 @@ return (
     <div class="row">
         <div class="col-10 center">
             <div class="card card-outline card-info">
-                <div class="card-header">
+                <div class="card-header text-white" >
                     <h3 class="card-title">Calendar</h3>
                 </div>
 
@@ -47,16 +54,21 @@ return (
                                       showNeighboringMonth={false}
                                       locale={"en-US"}
                                       class="calendarDiv"
-
                                     />
                                 </div>
                             </div>
-                            <div class="col-md center">
-                                <div>
+                            <div class="col-6">
+                                <div class="col-sm">
                                     <h5 class="calendarDiv">{calendarText}</h5>
-                                </div>
-                                <div>
-                                    Events
+<h5 class="calendarDiv">{categoryText}</h5>
+                                    <form>
+                                        <select class="custom-select mr-sm-2 bg-secondary cDropdown" onChange={onCategoryChange} >
+                                        <option value="" label="Categories">Categories</option>
+                                        <option value="concerts" label="Concerts">Concerts</option>
+                                        <option value="gatherings" label="Gatherings">Gathering</option>
+                                        <option value="others" label="Other">Others</option>
+                                        </select>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -76,7 +88,7 @@ return (
                     <div class="card-body  ">
                         <div id="Calendar_wrapper">
                             <div class="row">
-                                <div class="col-md center">
+                                <div class="p-3 mb-2 bg-secondary text-white">
                                event1
                                 </div>
                                 <div class="col-md center">
