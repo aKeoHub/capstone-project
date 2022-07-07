910dc789e59d32ed2cc967ddd30aeee084ffa6eb
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-07-06 19:38:03 -0600

    Update UsersController.java

--- src/main/java/com/project/capstone/users/UsersController.java
+++ src/main/java/com/project/capstone/users/UsersController.java
@@ -1,4 +1,41 @@
 package com.project.capstone.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @PostMapping("api/v1/user")
+    public Users createUsers(@Valid @RequestBody Users users) {
+        return usersService.saveUser(users);
+    }
+
+    @GetMapping("api/v1/user/{id}")
+    public Users getUser(@PathVariable("id") Integer id) throws UserNotFoundException {
+        Optional<Users> user = usersService.getUser(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    @GetMapping
+    public List<Users> fetchUsersList() {
+        return usersService.fetchUserList();
+    }
+
+    @DeleteMapping("api/v1/user/{id}")
+    public String deleteUsersById(@PathVariable("id") Integer userId){
+        usersService.deleteUserById(userId);
+        return "Deleted Successfully";
+    }
 }
