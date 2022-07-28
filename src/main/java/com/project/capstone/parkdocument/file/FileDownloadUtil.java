8e8e3854e220a26179cc0b78bf76144447af164e
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-07-27 18:13:52 -0600

    implemented file uploading

---
+++ src/main/java/com/project/capstone/parkdocument/file/FileDownloadUtil.java
@@ -0,0 +1,31 @@
+package com.project.capstone.parkdocument.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+public class FileDownloadUtil {
+    private Path foundFile;
+
+    public Resource getFileAsResource(String fileCode) throws IOException {
+        String fpath = "src/main/java/com/project/capstone/Files-Upload";
+        Path dirPath = Paths.get(fpath);
+
+        Files.list(dirPath).forEach(file -> {
+            if (file.getFileName().toString().startsWith(fileCode)) {
+                foundFile = file;
+                return;
+            }
+        });
+
+        if (foundFile != null) {
+            return new UrlResource(foundFile.toUri());
+        }
+
+        return null;
+    }
+}
