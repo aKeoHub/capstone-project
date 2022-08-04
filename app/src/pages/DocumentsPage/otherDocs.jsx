3de70f884ce3132b034415357dda035d42f58039
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-08-03 18:50:56 -0600

    added hyperlinks

--- app/src/pages/DocumentsPage/otherDocs.jsx
+++ app/src/pages/DocumentsPage/otherDocs.jsx
@@ -234,7 +234,9 @@ const OtherDocs = () => {
                              </div>
                          )}
 
-                       <DocModal />
+                      <div style={{textAlign: "center"}}> <DocModal /> </div>
+                        <tr><br></br></tr>
+                        <tr><br></br></tr>
 
                         <div className="mx-auto mb-4 px-4 py-4" style={{width: "980px", background: "#bcc1c4"}}>
                         <h1 className="text-secondary text-success mb-4"> Document Categories</h1>
