1aad64698a5f86804d2ad971ed3030e35849eb92
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-08-05 16:56:07 -0600

    added back end file delete

--- src/main/java/com/project/capstone/parkdocument/file/FilesController.java
+++ src/main/java/com/project/capstone/parkdocument/file/FilesController.java
@@ -12,11 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import com.project.capstone.parkdocument.file.FileInfo;
@@ -62,5 +58,12 @@ public class FilesController {
         Resource file = storageService.load(filename);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @DeleteMapping ("/api/v1/deleteFile/{filename:.+}")
+    public ResponseEntity<?> deleteFile(@PathVariable String filename) {
+        filename= URLDecoder.decode(filename, StandardCharsets.UTF_8).replaceAll("[\s|\u00A0]+", " ");
+        storageService.deleteFile(filename);
+        return ResponseEntity.ok().build();
     }
 }
