84bc08114601a28e1cd4e9e8b5e889376e419a25
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-08-07 13:40:43 -0600

    undid SQL Backend changes. Preping for AWS

--- src/main/java/com/project/capstone/CapstoneApplication.java
+++ src/main/java/com/project/capstone/CapstoneApplication.java
@@ -1,6 +1,12 @@
 package com.project.capstone;
 
 
+import com.project.capstone.category.CategoryService;
+import com.project.capstone.event.EventService;
+import com.project.capstone.parkdocument.ParkService;
+import com.project.capstone.user.User;
+import com.project.capstone.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 
 
 @SpringBootApplication
@@ -21,6 +28,27 @@ public class CapstoneApplication {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService, ParkService parkService, CategoryService categoryService, EventService eventService) {
+		return args -> {
+
+			userService.saveUser(new User( 0, "user", "1234", "user", "regular", "user@live.ca", 1, LocalDate.now()));
+			userService.saveUser(new User(0 , "admin", "1234", "admin", "admin", "admin@live.ca", 1, LocalDate.now()));
+			userService.saveUser(new User(0, "manager", "1234", "manager", "manager", "manager@live.ca", 1, LocalDate.now()));
+
+
+			userService.addRoleToUser("user", "ROLE_USER");
+			userService.addRoleToUser("admin", "ROLE_ADMIN");
+			userService.addRoleToUser("manager", "ROLE_MANAGER");
+
+
+
+
+
+
+		};
 	}
 
 
