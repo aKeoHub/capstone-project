63314b5b0a58019b5d19c77361ca0a136b401218
shiana-khehra <97817879+shiana-khehra@users.noreply.github.com>
2022-08-07 13:29:04 -0600

    added document backend javadoc

--- src/main/java/com/project/capstone/parkdocument/ParkDocument.java
+++ src/main/java/com/project/capstone/parkdocument/ParkDocument.java
@@ -16,7 +16,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Entity class representing the SQL Document Table
+ *
+ * @version 1.0
+ * @author Cole Humeniuk
+ */
 @Entity
 @Table(name = "park_document")
 @RequiredArgsConstructor
@@ -26,18 +31,26 @@ import java.util.List;
 @JsonDeserialize(as = ParkDocument.class)
 public class ParkDocument implements Serializable {
 
+    /**
+     */
     @Id
     @Column(name = "document_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonIdentityReference(alwaysAsId = true)
     private Integer documentId;
 
+    /**
+     * SQL Column
+     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "document_category", referencedColumnName = "category_id", nullable = false)
     @JsonIdentityReference(alwaysAsId = true)
     @ToString.Exclude
     private Category documentCategory;
 
+    /**
+     * SQL Column
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "creator_id", referencedColumnName = "user_id", nullable = false)
     @JsonIdentityReference(alwaysAsId = true)
@@ -45,25 +58,49 @@ public class ParkDocument implements Serializable {
     @JsonBackReference("docs")
     private User creatorId;
 
+    /**
+     * SQL Column
+     */
     @Column(name = "document_name", nullable = false, length = 30)
     private String documentName;
 
+    /**
+     * SQL Column
+     */
     @Column(name = "create_date")
     private LocalDate createDate;
 
+    /**
+     * SQL Column
+     */
     @Column(name = "description", nullable = false, length = 120)
     private String description;
 
+    /**
+     * SQL Column
+     */
     @Lob
     @Column(name = "file")
     private byte[] file;
 
+    /**
+     * SQL Column
+     */
     @OneToMany(mappedBy = "parkDocument", cascade = CascadeType.ALL)
     @JsonIgnore
     @ToString.Exclude
     private List<AuditLog> auditLogs = new ArrayList<>();
 
-
+    /**
+     * Required args Constructor assisting in Spring Annotations
+     * @param documentId
+     * @param documentCategory
+     * @param creatorId
+     * @param documentName
+     * @param createDate
+     * @param description
+     * @param file
+     */
     public ParkDocument(@JsonProperty("document_id") Integer documentId,
                         @JsonProperty("document_category") Category documentCategory,
                         @JsonProperty("creator_id") User creatorId,
@@ -80,66 +117,130 @@ public class ParkDocument implements Serializable {
         this.file = file;
     }
 
+    /**
+     * Getter for Audit logs
+     * @return auditLogs
+     */
     public List<AuditLog> getAuditLogs() {
         return auditLogs;
     }
 
+    /**
+     * Setter for Audit logs
+     * @param auditLogs
+     */
     public void setAuditLogs(List<AuditLog> auditLogs) {
         this.auditLogs = auditLogs;
     }
 
+    /**
+     * Getter for file
+     * @return file
+     */
     public byte[] getFile() {
         return file;
     }
 
+    /**
+     * Setter for file
+     * @param file
+     */
     public void setFile(byte[] file) {
         this.file = file;
     }
 
+    /**
+     * Getter for document description
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for document description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Getter for document createDate
+     * @return createDate
+     */
     public LocalDate getCreateDate() {
         return createDate;
     }
 
+    /**
+     * Setter for document createDate
+     * @param createDate
+     */
     public void setCreateDate(LocalDate createDate) {
         this.createDate = createDate;
     }
 
+    /**
+     * Getter for document name
+     * @return documentName
+     */
     public String getDocumentName() {
         return documentName;
     }
 
+    /**
+     * Setter for document name
+     * @param documentName
+     */
     public void setDocumentName(String documentName) {
         this.documentName = documentName;
     }
 
+    /**
+     * Getter for document creatorId
+     * @return creatorId
+     */
     public User getCreatorId() {
         return creatorId;
     }
 
+    /**
+     * Setter for document creatorId
+     * @param creatorId
+     */
     public void setCreatorId(User creatorId) {
         this.creatorId = creatorId;
     }
 
+    /**
+     * Getter for document category
+     * @return documentCategory
+     */
     public Category getDocumentCategory() {
         return documentCategory;
     }
 
+    /**
+     * Setter for document category
+     * @param documentCategory
+     */
     public void setDocumentCategory(Category documentCategory) {
         this.documentCategory = documentCategory;
     }
 
+    /**
+     * Getter for document id
+     * @return documentId
+     */
     public Integer getDocumentId() {
         return documentId;
     }
 
+    /**
+     * Setter for document id
+     * @param id
+     */
     public void setDocumentId(Integer id) {
         this.documentId = id;
     }
