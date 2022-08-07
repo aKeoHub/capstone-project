a1e0db03e2149bcae4162be71f28bd28cc15116d
CHumzz <86132515+CHumzz@users.noreply.github.com>
2022-08-07 10:56:38 -0600

    redid sql backend. CLR no longer needed

--- src/main/java/com/project/capstone/CapstoneApplication.java
+++ src/main/java/com/project/capstone/CapstoneApplication.java
@@ -19,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Optional;
 
 @SpringBootApplication
 public class CapstoneApplication {
@@ -38,26 +39,6 @@ public class CapstoneApplication {
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	};
-	@Bean
-	CommandLineRunner run(UserService userService, ParkService parkService, CategoryService categoryService, EventService eventService) {
-		return args -> {
-
-			userService.saveUser(new User( 0, "user", "1234", "user", "regular", "user@live.ca", 1, LocalDate.now()));
-			userService.saveUser(new User(0 , "admin", "1234", "admin", "admin", "admin@live.ca", 1, LocalDate.now()));
-			userService.saveUser(new User(0, "manager", "1234", "manager", "manager", "manager@live.ca", 1, LocalDate.now()));
-
-
-			userService.addRoleToUser("user", "ROLE_USER");
-			userService.addRoleToUser("admin", "ROLE_ADMIN");
-			userService.addRoleToUser("manager", "ROLE_MANAGER");
-
-
-
-
-
-
-		};
-	}
 
 }
 
